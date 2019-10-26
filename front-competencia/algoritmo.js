602fafced20b6b58dc850e774a9ecab258142c53
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-10-26 15:35:59 -0500

    jeje

--- front-competencia/algoritmo.js
+++ front-competencia/algoritmo.js
@@ -3,23 +3,22 @@ var listaYaPaso;
 
 
 
-function inicializarLista()
-{
+function inicializarLista() {
     let request = {
         origin: 'Colegio Italiano Leonardo Da Vinci, Bogota',
         destination: 'Colegio Italiano Leonardo Da Vinci, Bogota',
         travelMode: 'DRIVING',
-        waypoints: [{location: 'Calle 85 #23-10, Bogota'},
-                    {location: 'Calle 93 #19-10, Bogota'},
-                    {location: 'Calle 100 #19-10, Bogota'},
-                    {location: 'Calle 108 #15-10, Bogota'},
-                    {location: 'Calle 116 #20-10, Bogota'}]
+        waypoints: [{ location: 'Calle 85 #23-10, Bogota' },
+        { location: 'Calle 93 #19-10, Bogota' },
+        { location: 'Calle 100 #19-10, Bogota' },
+        { location: 'Calle 108 #15-10, Bogota' },
+        { location: 'Calle 116 #20-10, Bogota' }]
     };
 
-    directionsService.route(request, function(result, status){
+    directionsService.route(request, function (result, status) {
         console.log(result);
         lista = result;
-        if(status == 'OK') {
+        if (status == 'OK') {
             directionsRenderer.setDirections(result);
         }
     });
@@ -27,31 +26,28 @@ function inicializarLista()
 }
 
 
-function ya(b){
-    for(i=0; i<b ; i++){
-        listaYaPaso[i]=lista[i];
+function ya(b) {
+    for (i = 0; i < b; i++) {
+        listaYaPaso[i] = lista[i];
     }
 }
 
 
-function calcularPorcentaje(puntoActual, puntoFinalDefinido)
-{
-    return (distanciaRecorrida(puntoActual)/distanciaTotal()) * 100;
+function calcularPorcentaje(puntoActual, puntoFinalDefinido) {
+    return (distanciaRecorrida(puntoActual) / distanciaTotal()) * 100;
 }
 
-function distanciaRecorrida(puntoActual)
-{
-    dist =0;
-    for(i = 0 ; i < listaYaPaso.routes[0].legs.length; i++){
+function distanciaRecorrida(puntoActual) {
+    dist = 0;
+    for (i = 0; i < listaYaPaso.routes[0].legs.length; i++) {
         dist = dist + listaYaPaso.routes[0].legs[i].distance.value;
     }
     return dist
 }
 
-function distanciaTotal()
-{
-    dist =0;
-    for(i = 0 ; i < lista.routes[0].legs.length; i++){
+function distanciaTotal() {
+    dist = 0;
+    for (i = 0; i < lista.routes[0].legs.length; i++) {
         dist = dist + lista.routes[0].legs[i].distance.value;
     }
     return dist
@@ -70,13 +66,13 @@ function estaDentroCamino(punto)
     return dentro
 } */
 
-function calcularaListas(){
+function calcularaListas() {
 
 }
 
 /***
  * Modifica Json (BONO)
  */
-function calcularNuevaRuta(puntoActual, puntoFinal){
+function calcularNuevaRuta(puntoActual, puntoFinal) {
 
 }
