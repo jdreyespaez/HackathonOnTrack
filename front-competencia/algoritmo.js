54cfb0dae06daf03ff776890d606ae0ea29a09c5
CamGitH <camilo.orgc@gmail.com>
2019-10-26 14:09:46 -0500

    0.1

---
+++ front-competencia/algoritmo.js
@@ -0,0 +1,35 @@
+var lista;
+var listaYaPaso;
+
+function inicializarLista(pLista)
+{
+    lista = pLista;
+}
+
+function calcularPorcentaje(puntoActual, puntoFinalDefinido)
+{
+    return (distanciaRecorrida(puntoActual)/distanciaTotal()) * 100;
+}
+
+function distanciaRecorrida(puntoActual)
+{
+
+}
+
+function distanciaTotal()
+{
+
+}
+
+function estaDentroCamino(punto)
+{
+
+}
+
+/***
+ * Modifica Json
+ */
+function calcularNuevaRuta(puntoActual, puntoFinal)
+{
+
+}
