1018faeb029266ed1d0ef2a1849e82cfa7467343
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-10-28 11:05:52 -0500

    Accepted changes

--- front-competencia/algoritmo.js
+++ front-competencia/algoritmo.js
@@ -3,11 +3,11 @@ var listaYaPaso;
 var listafalta;
 var listo;
 
-listo = [{location: 'Calle 85 #23-10, Bogota'},
-{location: 'Calle 93 #19-10, Bogota'},
-{location: 'Calle 100 #19-10, Bogota'},
-{location: 'Calle 108 #15-10, Bogota'},
-{location: 'Calle 116 #20-10, Bogota'}];
+listo = [{ location: 'Calle 85 #23-10, Bogota' },
+{ location: 'Calle 93 #19-10, Bogota' },
+{ location: 'Calle 100 #19-10, Bogota' },
+{ location: 'Calle 108 #15-10, Bogota' },
+{ location: 'Calle 116 #20-10, Bogota' }];
 
 listafalta = listo;
 
@@ -48,9 +48,9 @@ function inicializarLista() {
 }
 
 
-function ya(b){
-    for(i=0; i<b ; i++){
-        listaYaPaso[i]=listo[i];
+function ya(b) {
+    for (i = 0; i < b; i++) {
+        listaYaPaso[i] = listo[i];
         listafalta.shift();
 
     }
@@ -62,16 +62,9 @@ function calcularPorcentaje(puntoActual, puntoFinalDefinido) {
 }
 
 
-<<<<<<< HEAD
 function distanciaRecorrida(puntoActual) {
     dist = 0;
     for (i = 0; i < listaYaPaso.routes[0].legs.length; i++) {
-=======
-function distanciaRecorrida(puntoActual)
-{
-    dist =0;
-    for(i = 0 ; i < listaYaPaso.routes[0].legs.length; i++){
->>>>>>> acfc3c32ddd67ca55be6fd838fb5bc6b6839f5fc
         dist = dist + lista.routes[0].legs[i].distance.value;
 
     }
@@ -97,22 +90,14 @@ function estaDentroCamino(punto)
         dentro = true;
     }
     return dentro
-<<<<<<< HEAD
-=======
-} */
-
-function calcularaListas() {
-
->>>>>>> 8bb66011e38ef3b417e7c3b2a4b7aa9aa0e17474
 }
  */
 
 /***
  * Modifica Json (BONO)
  */
-<<<<<<< HEAD
-function calcularNuevaRuta(puntoActual){
-  
+function calcularNuevaRuta(puntoActual) {
+
 
     let request = {
         origin: puntoActual,
@@ -120,13 +105,10 @@ function calcularNuevaRuta(puntoActual){
         travelMode: 'DRIVING',
         waypoints: listafalta
     };
-=======
-function calcularNuevaRuta(puntoActual, puntoFinal) {
->>>>>>> 8bb66011e38ef3b417e7c3b2a4b7aa9aa0e17474
 
-    directionsService.route(request, function(result, status){
+    directionsService.route(request, function (result, status) {
         console.log(result);
-        if(status == 'OK') {
+        if (status == 'OK') {
             directionsRenderer.setDirections(result);
         }
     });
